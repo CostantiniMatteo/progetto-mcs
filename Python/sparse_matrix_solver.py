0e8b866cb4ec8729c44a4a8335d5e5d1fccab999
Matteo Costantini <matteo.angelo.costantini@gmail.com>
2018-04-25 14:09:19 +0200

    Update python script

--- Python/sparse_matrix_solver.py
+++ Python/sparse_matrix_solver.py
@@ -5,14 +5,13 @@ from os.path import isfile, join, exists, abspath
 from datetime import datetime
 
 import scipy, scipy.io
-from scipy.sparse.linalg import spsolve, norm
+from scipy.sparse.linalg import norm, spsolve
 from scipy.linalg import norm
 
 from memory_profiler import memory_usage, profile
 
 
-
-MAT_DIR = abspath('../Matrices')
+MAT_DIR = abspath('../Matrices/positive_definite')
 
 def solve(A):
     # Compute b such that the exact solution of Ax = b is xe = [1, 1, ...]
@@ -24,7 +23,7 @@ def solve(A):
     # Solve Ax = b
     start_time = datetime.now()
     x = solve_aux(A, b)
-    time = datetime.now() - start_time
+    t = datetime.now() - start_time
 
     # || xe - x || / ||xe||
     relative_error = norm(x - xe, 2) / norm(xe, 2)
@@ -34,13 +33,15 @@ def solve(A):
         print("Non-zero: {} (< 0.00001%)".format(A.nnz))
     else:
         print("Non-zero: {0} ({1:.5f}%)".format(A.nnz, p_non_zero))
-    print("Time: {}.{}s".format(time.seconds, time.microseconds))
     print("Relative error: {}".format(relative_error))
+    print(f"Time (solve): {t} ({t.seconds}.{t.microseconds}s)")
+
 
 
 @profile
 def solve_aux(A, b):
-    return spsolve(A, b)
+    x = spsolve(A, b, use_umfpack=False)
+    return x
 
 
 
@@ -54,8 +55,10 @@ if __name__ == "__main__":
     for f in sorted(listdir(MAT_DIR)):
         path = join(MAT_DIR, f)
         if isfile(path):
-            print("===== Solving {} =====".format(f))
+            print("================== Solving {} ==================".format(f))
+            start_time = datetime.now()
             A = scipy.io.mmread(path).tocsc()
+            t = datetime.now() - start_time
             x = solve(A)
+            print(f"Time (read): {t} ({t.seconds}.{t.microseconds}s)")
             print('')
-
