29f1a44ecc0fb17608a1edbe5108344b94417e2c
Matteocolella95 <m.colella2@campus.unimib.it>
2018-05-02 15:53:00 +0200

    Fatte modifiche script

--- MATLAB/sparse_matrix_solver.m
+++ MATLAB/sparse_matrix_solver.m
@@ -1,26 +1,30 @@
 clear all;
 clc;
 
-MAT_DIR = 'Matrices/positive_definite/';
-% Carico lista delle matrici da risolvere
-list = dir(MAT_DIR);
-list = arrayfun(@(item) item.name, list, 'UniformOutput', false);
+% MAT_DIR = 'Matrices/positive_definite/';
+% % Carico lista delle matrici da risolvere
+% list = dir(MAT_DIR);
+% list = arrayfun(@(item) item.name, list, 'UniformOutput', false);
 
 
-for i = 1:length(list)
-    curr_file = list{i};
-    if ~endsWith(curr_file, '.mat')
-        continue
-    end
-    
-    % Carico matrice dei coefficienti A
-    mat = load(fullfile(MAT_DIR, curr_file));
-    solve_matrix(mat);
-    
-end
+% for i = 1:length(list)
+%     curr_file = list{i};
+%     if ~endsWith(curr_file, '.mat')
+%         continue
+%     end
+%     
+%     % Carico matrice dei coefficienti A
+%     mat = load(fullfile(MAT_DIR, curr_file));
+%     solve_matrix(mat);
+%     
+% end
+
+pathName = input('');
+mat = load(pathName);
+input('Matrice caricata. Premi invio per continuare');
+solve_matrix(mat);
 
 function solve_matrix(mat)
-    
     
     % Inizializzo il profiler per l'utilizzo di memoria
     profile clear
@@ -35,6 +39,7 @@ function solve_matrix(mat)
     tic;
     x = A\b;
     t = toc;
+    input('Sistema risolto. Premi invio per continuare');
 
     % Salvo informazioni riguardo l'utilizzo di memoria
     p = profile('info');
@@ -43,16 +48,31 @@ function solve_matrix(mat)
     % Calcolo errore relativo
     relative_error = norm(x - xe)/norm(xe);
 
+    [fileID, errormsg] = fopen('../logs/matlab.log', 'at+');
+%     fprintf(fileID, 'name,dim,nnz,re,time,memory,positive,lang,os\n');
+    fprintf(fileID, '%s', mat.Problem.name, ', ');
+    fprintf(fileID, '%s', num2str(size(mat.Problem.A, 1)), ', ');
+    fprintf(fileID, '%s', num2str(nnz(mat.Problem.A)), ', ');
+    fprintf(fileID, '%s', num2str(relative_error), ', ');
+    fprintf(fileID, '%s', num2str(t), ', ');
+    fprintf(fileID, '%s', num2str(mem_usage), ', ');
+%     fprintf(fileID,); Positive/Negative
+    fprintf(fileID, 'matlab, ');
+%     fprintf(fileID,); OS
+    fprintf(fileID, '\n');
+
+    fclose(fileID);
+
     %  Visualizzo informazioni matrice e report
-    disp(['Name: ', mat.Problem.name]);
-    disp(['Allocated memory: ',num2str(mem_usage),' Byte']);
-    disp(['Dimensions: ', num2str(size(mat.Problem.A, 1)), ' x ', ...
-        num2str(size(mat.Problem.A, 2))]);
-    disp(['Number of elements: ', num2str(nnz(mat.Problem.A)), ...
-        ' (', num2str(100*(nnz(mat.Problem.A)/ ...
-        (size(mat.Problem.A, 1)*size(mat.Problem.A, 1)))), '%)']);
-    disp(['Relative Error: ', num2str(relative_error)]);
-    disp(['System solving time: ', num2str(t),' s']);
-    fprintf('\n\n');
+%     disp(['Name: ', mat.Problem.name]);
+%     disp(['Allocated memory: ',num2str(mem_usage),' Byte']);
+%     disp(['Dimensions: ', num2str(size(mat.Problem.A, 1)), ' x ', ...
+%         num2str(size(mat.Problem.A, 2))]);
+%     disp(['Number of elements: ', num2str(nnz(mat.Problem.A)), ...
+%         ' (', num2str(100*(nnz(mat.Problem.A)/ ...
+%         (size(mat.Problem.A, 1)*size(mat.Problem.A, 1)))), '%)']);
+%     disp(['Relative Error: ', num2str(relative_error)]);
+%     disp(['System solving time: ', num2str(t),' s']);
+%     fprintf('\n\n');
    
 end
