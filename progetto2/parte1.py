c79c2d530b607980585699c24418216497e91172
Matteo Costantini <matteo.angelo.costantini@gmail.com>
2018-05-21 14:15:10 +0200

    -

--- progetto2/parte1.py
+++ progetto2/parte1.py
@@ -5,6 +5,17 @@ from tqdm import tqdm
 from scipy.fftpack import dct, idct
 
 
+
+TEST_MAT = np.array([[231, 32, 233, 161, 24, 71, 140, 245],
+                     [247, 40, 248, 245, 124, 204, 36, 107],
+                     [234, 202, 245, 167, 9, 217, 239, 173],
+                     [193, 190, 100, 167, 43, 180, 8, 70],
+                     [11, 24, 210, 177, 81, 243, 8, 112],
+                     [97, 195, 203, 47, 125, 114, 165, 181],
+                     [193, 70, 174, 167, 41, 30, 127, 245],
+                     [87, 149, 57, 192, 65, 129, 178, 228]])
+
+
 def custom_alfa(u, N):
     if u == 0:
         return math.sqrt(1/N)
@@ -37,19 +48,32 @@ def custom_dct2(mat):
 
     return r_mat.T
 
+def scipy_dct2(mat):
+    return dct(dct(mat.T, norm='ortho').T, norm='ortho')
 
 
-mat = np.array([[ 95, 251, 154, 130,  89],
-                [209,  65,  50, 183, 224],
-                [ 38, 157,  31,  35, 131],
-                [  7, 137, 113, 118,  81],
-                [138,  32,  95, 132, 185]])
 
-test_mat = np.array([[231, 32, 233, 161, 24, 71, 140, 245],
-                  [247, 40, 248, 245, 124, 204, 36, 107],
-                  [234, 202, 245, 167, 9, 217, 239, 173],
-                  [193, 190, 100, 167, 43, 180, 8, 70],
-                  [11, 24, 210, 177, 81, 243, 8, 112],
-                  [97, 195, 203, 47, 125, 114, 165, 181],
-                  [193, 70, 174, 167, 41, 30, 127, 245],
-                  [87, 149, 57, 192, 65, 129, 178, 228]])
+if __name__ == '__main__':
+    import sys, cv2
+    from datetime import datetime
+    from os import listdir
+
+    PATH = 'immagini/grey'
+    if len(sys.argv > 1):
+        PATH = sys.argv[1]
+
+    for file in tqdm(os.listdir(PATH)):
+        if file.endswith('.bmp'):
+            img = cv2.imread('immagini/artificial.bmp')
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+            t_start = datetime.now()
+            custom_res = custom_dct2(img)
+            end_custom = (datetime.now() - t_start).total_seconds()
+
+            t_start = datetime.now()
+            scipy_res = scipy_dct2(img)
+            end_scipy = (datetime.now() - t_start).total_seconds()
+
+            print(f"{file.split('.')[0]},{end_custom},{end_scipy}")
+
