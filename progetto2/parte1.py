fixed *high* computation issues

progetto2/parte1.py
@@ -15,18 +15,18 @@ TEST_MAT = np.array([[231,  32, 233, 161,  24,  71, 140, 245],
                      [ 87, 149,  57, 192,  65, 129, 178, 228]])
 
 
-def custom_dct(array, a):
+def custom_dct(array):
     r_array = np.zeros(array.size)
     N = array.size
 
     for u in range(N):
         somma = 0
-        alfa = a if u == 0 else 2 * a
+        a = math.sqrt(1. / N) if u == 0 else math.sqrt(2. / N)
 
         for x, cell in enumerate(array):
             somma += cell * math.cos( (u * math.pi * (2 * x + 1)) / (2 * N) )
 
-        r_array[u] = alfa*somma
+        r_array[u] = a * somma
 
     return r_array
 
@@ -34,21 +34,10 @@ def custom_dct(array, a):
 def custom_dct2(mat):
     r_mat = np.zeros(mat.shape)
 
-    r_mat = np.apply_along_axis(
-        custom_dct,
-        axis=1,
-        arr=mat,
-        a=math.sqrt(1/mat.shape[1])
-    )
+    r_mat = np.apply_along_axis(custom_dct, axis=1, arr=mat)
+    r_mat = np.apply_along_axis(custom_dct, axis=0, arr=r_mat)
 
-    r_mat = np.apply_along_axis(
-        custom_dct,
-        axis=1,
-        arr=r_mat.T,
-        a=math.sqrt(1/mat.shape[0])
-    )
-
-    return r_mat.T
+    return r_mat
 
 
 def scipy_dct2(mat):
@@ -70,22 +59,15 @@ if __name__ == '__main__':
     for file in sorted(listdir(PATH)):
         if file.endswith('.bmp'):
             img = cv2.imread(abspath(join(PATH, file)))
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-            t_start = datetime.now()
-            scipy_res = scipy_dct2(img)
-            end_scipy = (datetime.now() - t_start).total_seconds()
+            # Slow if ints for some unknown reason
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY).astype(float)
 
             t_start = datetime.now()
             custom_res = custom_dct2(img)
             end_custom = (datetime.now() - t_start).total_seconds()
 
+            t_start = datetime.now()
+            scipy_res = scipy_dct2(img)
+            end_scipy = (datetime.now() - t_start).total_seconds()
+
             print(f"{file.split('.')[0]},\t{end_custom},\t{end_scipy}")
-
-    # for i in range(100, 500, 100):
-    #     img = np.random.uniform(low=0, high=255, size=(i,i))
-
-    #     t_start = datetime.now()
-    #     custom_res = custom_dct2(img)
-    #     end_custom = (datetime.now() - t_start).total_seconds()
-    #     print(f"{i}: {end_custom}s")